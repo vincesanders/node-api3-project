624a07e6d5fed39d8fdd661780e74d669410fbff
Vincent Sanders <vincesanders@hotmail.com>
2020-02-12 13:54:47 -0600

    Set up server. Added logger and user validation middlewar. Added functionality to create new users, get all users and get users by id.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
+const userRouter = require('./users/userRouter');
 
 const server = express();
+
+server.use(express.json(), logger, helmet());
+
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +14,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get('Origin')}`);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,83 @@
 const express = require('express');
 
+const database = require('./userDb');
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res) => {
+  database.insert(req.body).then(userIDObj => {
+      //returns an object containing the id of the user just created.
+      res.status(201).json(userIDObj);
+  }).catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: 'oops' });
+  });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validatePost, validateUserId, (req, res) => {
   // do your magic!
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  database.get().then(users => {
+    res.status(200).json(users);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ errorMessage: "The users information could not be retrieved." });
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
+});
+
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUser, validateUserId, (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  database.getById(req.params.id).then(user => {
+    if (!user) {
+        res.status(400).json({ message: "invalid user id" });
+    } else {
+      req.user = user;
+      next();
+    }
+  }).catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "The user information could not be retrieved." });
+  });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing user data" });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing post data" });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
 }
 
 module.exports = router;
